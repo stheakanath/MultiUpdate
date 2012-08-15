7b66d7a914892308d6055f11efe700dc85301aa0
Sony Theakanath <stheakanath@gmail.com>
2012-08-15 09:40:01 -0700

    Initial Commit

---
+++ MultiUpdate/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MultiUpdate
+//
+//  Created by Sony Theakanath on 8/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class MainViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) MainViewController *mainViewController;
+
+@end
---
+++ MultiUpdate/MainViewController.h
@@ -0,0 +1,15 @@
+//
+//  MainViewController.h
+//  MultiUpdate
+//
+//  Created by Sony Theakanath on 8/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "FlipsideViewController.h"
+
+@interface MainViewController : UIViewController <FlipsideViewControllerDelegate>
+
+- (IBAction)showInfo:(id)sender;
+
+@end
